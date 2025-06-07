1fdc5c7c5af9fd06201c114bc3484ffbaad6ded9
Rahul Kerudi <rahulkerudi@gmail.com>
2025-06-07 20:27:43 +0530

    Updated main.py

--- frontend/main.py
+++ frontend/main.py
@@ -3,8 +3,8 @@ from add_update_ui import add_update
 from analytics_by_category_ui import get_analytics
 from analytics_by_month_ui import get_analytics_by_month
 
-st.set_page_config(page_title="Expense Tracking System", page_icon="../images/EPS.png")
-st.image("../images/EPS.png",use_container_width=True)
+st.set_page_config(page_title="Expense Tracking System", page_icon="images/EPS.png")
+st.image("images/EPS.png",use_container_width=True)
 
 
 tab1,tab2,tab3 = st.tabs(["Add/Update",'Analytics By Category','Analytics By Month'])
